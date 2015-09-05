1caa372dfb9612ba79c06ffd5ec60ee4a17eaea8
Herb Miller <herb@bobbingwide.com>
2015-09-05 11:57:21 +0100

    dinlo v0.0.1 - added some comments
    Wrote some comments to give a little advice in how to change the code
    to fix some problems, introduce new ones, and fix these as well.

--- dinlo.php
+++ dinlo.php
@@ -12,57 +12,88 @@ License: GPL2
 
 */
 
+/**
+ * Initially we run the dinlo hook on admin_init
+ * this will produce a number of Notices and Warnings.
+ * Fix this by commenting out the add_action
+ *
+ * Create a new problem by uncommenting the "admin_menu" action.
+ * This will produce debug stuff to the page. 
+ * It will mess up the WordPress Dashboard menu.
+ * When using oik-bwtrace you can replace these calls with the APIs shown
+ *
+ * There's problems with the other two hook functions. 
+ */
 add_action( "admin_init", "dinlo" );
-add_action( "admin_menu", "dinlo_am" ); 
+//add_action( "admin_menu", "dinlo_am" ); 
 add_action( "shutdown", "dinlo_shutdown" );
 add_filter( "wp_die_ajax_handler", "dinlo_die" );
 add_filter( "update_footer", "dinlo_update_footer", 1 );
 
-
 /**
  * Be a dinlo
  *
- *
  * Attempt to get the value of page, which isn't set on some admin pages
  * such as Plugins, but will be set on the Settings > oik trace options page.
- * If unsuccessful then cause a load of other problems
+ * Attempt to hide Notices using the @ operator.
+ * 
+ * If unsuccessful in getting the 'page' then cause a couple of other of niggles.
+ * The divide by zero problem only occurs when WP_DEBUG is true.
  */
 function dinlo() {
 	if ( !@$_GET['page'] ) {
 		$dinlo = PI;
-		$dinlo /= 0;
+		if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
+			$dinlo /= 0;
+		}
 	}
 }
 
 /**
- * Mess up an AJAX request 
+ * Produce some simple debugging output
+ * 
+ * This is one way of getting debug output, but it messes up the display.
+ *
+ * It's much better to replace the calls with the following:
+ * `
+ * bw_trace2( $_SERVER );
+ * bw_backtrace();
+ * `
  */
 function dinlo_am() {
-	echo "<!-- Hello Dinlo -->";
+	var_dump( $_SERVER );
+	print_r( debug_backtrace() );
 }
-
 
 /**
  * Output some rubbish to the browser
  */
 function dinlo_shutdown() {
-	var_dump( $_SERVER );
-	print_r( debug_backtrace() );
+	echo "<!-- Hello Dinlo -->";
 }
 
 /**
  * Change the footer badly
  *
  * It's supposed to be done by updating $update
- * and returning the value.
+ * and returning the value... it's a filter function, not an action hook.
  */
 function dinlo_update_footer( $update ) {
   echo "Hello Dinlo $update";
 }
 
+/**
+ * See if we can mess up AJAX
+ *
+ * Here's an example of bw_trace2() that will only log the trace record
+ * when tracing at "Debug" level.
+ *
+ * @param string $ajax_handler 
+ * @return string updated AJAX die handler function
+ */
 function dinlo_die( $ajax_handler ) {
 	bw_trace2( null, null, true, BW_TRACE_ERROR );
-  return( "dinlo_am" );
+  return( "dinlo_not_there" );
 }
 
 
